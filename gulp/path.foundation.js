931e44fd08833c3986f6b7d2c9a2e162d769b777
visermort <oxygenn@list.ru>
2016-03-02 11:00:39 +0300

    Made changes in .gitignore

--- gulp/path.foundation.js
+++ gulp/path.foundation.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = [
-  './bower_components/jquery/dist/jquery.min.js',
-  './bower_components/blueimp-file-upload/js/jquery.fileupload.js'
+  './bower_components/jquery/dist/jquery.min.js'
 
 ];
