6d1e094a02ab59912e43d3b5b97f9d0b553aca2e
visermort <oxygenn@list.ru>
2016-03-04 23:23:22 +0300

    Generate watemark in scale

--- source/tobuild/assets/php/filedownload.php
+++ source/tobuild/assets/php/filedownload.php
@@ -6,19 +6,6 @@ include ('lib/abeautifulsite/SimpleImage.php');
 include ('settings.php');
 
 
-function getFileToDownload($fileName) {
-// Контент-тип означающий скачивание
-    header("Content-Type: application/octet-stream");
-// Размер в байтах
-    header("Accept-Ranges: bytes");
-// Размер файла
-    header("Content-Length: ".filesize($fileName));
-// Расположение скачиваемого файла
-    header("Content-Disposition: attachment; filename=".$fileName);
-// Прочитать файл
-    readfile($fileName);
-}
-
 session_start(); //обращение к сессии
 
 if (!$_SESSION or !$_SESSION['imageFile'] or !$_SESSION['watermarkFile']) {
@@ -33,13 +20,30 @@ if (!$_SESSION or !$_SESSION['imageFile'] or !$_SESSION['watermarkFile']) {
 $mainImage = new abeautifulsite\SimpleImage($settings['imageShortPath'] . $_SESSION['imageFile']);
 $watermarkImage = new abeautifulsite\SimpleImage($settings['imageShortPath'] . $_SESSION['watermarkFile']);
 
-$opacity = 0.5;
-$left = 0;
-$top = 0;
 //получить данные из пост
 
-//далее  $watermarkImage нужно ресайзить в соответсвии с тем, что получили из POST
+$opacity = 0.7;
+if ($_POST['opacity'])  $opacity = $_POST['opacity'];
+$left = $_POST['left'];
+$top =  $_POST['top'];
+$imageWidth = $_POST['imgWidth'];
+$watemarkWidth = $_POST['watemarkWidth'];
 
+//далее  $watermarkImage нужно ресайзить в соответсвии с тем, что получили из POST
+$width = $mainImage -> get_width();
+$wmWidth = $watermarkImage -> get_width();
+$mainImageScale = $width / $imageWidth;//получили коэффициент картинка на столько больше чем у нас на экране
+//на эту величину умножим top left;
+$left = $left * $mainImageScale;
+$top = $top * $mainImageScale;
+//разбираемся в вотермарком
+$watermarkScale = 1;
+if ( $wmWidth > $watemarkWidth ) $watermarkScale = $wmWidth / $watemarkWidth; //коэффициент, насколько реальный больше, чем на экране
+
+if (!($mainImageScale == 1 and $watermarkScale == 1) ) {  //если нужно , то ресайзим
+    $newWidth = $mainImageScale * $watemarkWidth / $watermarkScale;
+    $watermarkImage -> fit_to_width( $newWidth );
+}
 
 
 //сформировать новое имя файла
@@ -52,4 +56,16 @@ $mainImage -> overlay($watermarkImage, 'top left', $opacity, $left, $top ) ->  s
 
 exit( json_encode(array( 'status' => true ,
     'url' =>$settings['phpPath'].$newFileName ,
-    'message' => 'Склейка изображения выполнена' )));
+    'message' => 'Склейка изображения выполнена', //далее это всё выводим для отладки
+    'with' =>  $imageWidth,
+    'watermarkwidth' => $watemarkWidth,
+    'realwidth' => $width,
+    'realwatermarkWidth' => $wmWidth,
+    'top' => $top,
+    'left' => $left,
+    'scale' =>  $mainImageScale,
+    'watermarkScale' => $watermarkScale,
+    'newWidth' => $newWidth,
+    'opacity' => $opacity
+
+)));
--- source/tobuild/assets/php/utils.php
+++ source/tobuild/assets/php/utils.php
@@ -2,10 +2,44 @@
 
 include ('settings.php');
 
+
+
+//отправка файла на скачивание
+function getFileToDownload($fileName) {
+    // Контент-тип означающий скачивание
+    header("Content-Type: application/octet-stream");
+    // Размер в байтах
+    header("Accept-Ranges: bytes");
+    // Размер файла
+    header("Content-Length: ".filesize($fileName));
+    // Расположение скачиваемого файла
+    header("Content-Disposition: attachment; filename=".$fileName);
+    // Прочитать файл
+    readfile($fileName);
+}
+
+$url = $_POST['url'];
+
+if (!$url){
+    header("content-type: application/json");
+    exit( json_encode(array(
+        'status' => false ,
+        'message' => 'Нет url файла для закачки!'
+    )));
+}
+//из пути к файлу вырезать лишнюю часть
+if ( stristr( $url , $settings['phpPath'] )) {
+    $url = substr($url, strlen($settings['phpPath'])) ;
+}
+
+//getFileToDownload($url); //отправляем на закачку
+
+
 header("content-type: application/json");
-
-
 exit( json_encode(array(
     'status' => true ,
-    'message' => 'выдача на скачивание или очистка сесси - всё прошло хорошо'
+    'message' => 'Выдача файла на скачивание - ОК',
+    'url' => $url
 )));
+
+//Сразу вопрос - Где должен быть показ картинки результата? В окне барузера на полный экран?  И не лучше ли вначале показать картинку, и потом уже (если она понравилась) пользователь жмёт на "сохранить", а не скачать, потом увидеть?
